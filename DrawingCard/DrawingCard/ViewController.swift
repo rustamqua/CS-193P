d87f68d4aaeae0a77db008202619eb84e2a5bc64
Rustam-Deniz Emirali <emirali.rustam.deniz@gmail.com>
2020-06-18 19:32:26 +0600

    swipe getsure

--- DrawingCard/DrawingCard/ViewController.swift
+++ DrawingCard/DrawingCard/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	var deck = PlayingCardDeck()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
-
-
+	
+	@IBOutlet weak var playingCardView: DrawnCard!{
+		didSet{
+			let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+			swipe.direction = [.left, .right]
+			playingCardView.addGestureRecognizer(swipe)
+		}
+	}
+	
+	@IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+		playingCardView.isFaceUp = !playingCardView.isFaceUp
+	}
+	@objc
+	func nextCard() {
+		if let card = deck.draw(){
+			playingCardView.rank = card.rank.order
+			playingCardView.suit = card.suit.rawValue
+		}
+	}
 }
 
